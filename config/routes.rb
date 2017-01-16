7e5ce0a1577ad39d070b8eade0758547d5e7ad17
DAVID <davidnmwood@gmail.com>
2017-01-16 14:56:55 -0500

    Setup index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :setlists
+  get 'setlist/index'
+  root 'setlist#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
