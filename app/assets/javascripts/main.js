d5924cb2bbf243b04edb25dd75e2d7e0c09aec19
DAVID <davidnmwood@gmail.com>
2017-01-20 23:47:02 -0500

    Create JS function to add list of songs to show page

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,6 +1,13 @@
-$(function() {
-  $('#addButton').on('click', function() {
-    window.alert('button was clicked!')
+// $(document).ready(function () {
+// });
 
+$(function() {
+  $('#addButton').on('click', function(e) {
+    e.preventDefault();
+    var textInputVal = document.getElementById('textInput').value;
+    // var s = ( '#list' ).append( textInputVal );
+    $( '.list' ).append('<li>' + textInputVal + '</li>')
+    // console.log($( '.list' ))
+    // $('textInputVal').appendTo('list')
   });
 });
