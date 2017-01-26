1b40122706f4c2b8724c50f44a6a14b9ed2e272a
DAVID <davidnmwood@gmail.com>
2017-01-26 18:05:03 -0500

    change root page to #new

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
     resources :songs
   end
 
-  root 'setlist#index'
+  root 'setlist#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
