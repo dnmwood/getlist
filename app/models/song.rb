ba5733682280d299d73f3517b50c8d0d62a0a0ba
DAVID <davidnmwood@gmail.com>
2017-02-22 18:02:30 -0500

    CSS styling, experimenting with JS library 'sortable'

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,5 @@
 class Song < ApplicationRecord
   belongs_to :setlist
 
-  validates :title, length: { minimum: 1, message: "CUSTOM ERROR MESSAGE" }
+  validates :title, length: { minimum: 1, message: "ERROR MESSAGE" }
 end
